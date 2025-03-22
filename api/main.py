7f37e03613c5c3a68c704514b6ac2c71a37e65ea
kushabarodekar <31389189+kushabarodekar@users.noreply.github.com>
2025-03-22 21:03:07 +0530

    Test Token

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+API_KEY = os.getenv("AIPROXY_TOKEN")
+
 app = FastAPI()
 
 @app.get("/")
 async def health_check():
-    return "Up & Running Successfully"
+    return "Up & Running Successfully "+API_KEY
